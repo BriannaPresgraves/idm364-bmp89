e803c9a0f0c1db23d4d18363c5b5085f1ab03374
Brianna Presgraves <114011497+BriannaPresgraves@users.noreply.github.com>
2025-02-04 20:38:26 -0500

    added cart

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,3 +1,26 @@
+<script>
+    import Cart from '$lib/imgs/cart.png';
+</script>
+
 <header>
-    <h1>Artistry Hub</h1>
+    <h1 class="shop">Artistry Hub</h1>
+    <button class="cart">
+        <img src={Cart} alt="cart" width="29" height="29.02">
+    </button>
 </header>
+
+<style>
+
+.cart {
+    height: 50px;
+    width: 50px;
+    display: flex;
+    background-color: #e2f4fc;
+    border: none;
+    align-items: center;
+    justify-content: center;
+    margin: 0.9rem;
+    margin-right: 1rem;
+    margin-left: auto;
+}
+</style>
