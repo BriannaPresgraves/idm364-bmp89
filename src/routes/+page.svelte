d1a4c1657d8b20f36fd8b2bb7d6a2a79b7d082be
Brianna Presgraves <114011497+BriannaPresgraves@users.noreply.github.com>
2025-02-05 14:35:56 -0500

    display prod info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,46 @@
+<script>
+    import {products} from '$lib/utils.js';
+    import ProdCard from "$lib/ProdCard.svelte";
 
+    const groupedProducts = products.reduce((acc, product) => {
+            if (!acc[product.type]) acc[product.type] = [];
+            acc[product.type].push(product);
+            return acc;
+        }, {});
+</script>
+
+<div class="shop">
+    {#each Object.keys(groupedProducts) as type}
+        <div class="prodtype">
+            <h1>{type}</h1>
+        </div>
+        <div class="prodcards">
+            {#each groupedProducts[type] as product}
+                <ProdCard {product} />
+            {/each}
+        </div>
+    {/each}
+    </div>
+    
+    <style>
+        .shop {
+
+        }
+
+        .prodtype {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+            width: 100%;
+        }
+
+        .prodcards {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+            width: 100%;
+        }
+    
+    </style>
