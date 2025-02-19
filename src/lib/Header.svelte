d63e1e2589e17450885c4ce7ff578c821cc98d6c
Brianna Presgraves <114011497+BriannaPresgraves@users.noreply.github.com>
2025-02-19 00:47:24 -0500

    set up cart page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,15 +1,31 @@
 <script>
-    import cart from '$lib/imgs/cart.png';
+    import carts from '$lib/imgs/cart.png';
+    import { goto } from '$app/navigation';
+    import { cart } from '$lib/stores/cart';
+    import { derived } from 'svelte/store';
+
+    function goToCart() {
+        goto('/cart');
+    }
+
+    function goToHome() {
+        goto('/');
+    }
+
+    const hasItemsInCart = derived(cart, $cart => $cart.length > 0);
 </script>
 
 <header>
-    <h1 class="shop">Artistry Hub</h1>
-    <button class="cart">
-        <img src={cart} alt="cart" width="29" height="29.02">
+    <h1 on:click={goToHome} class="shop">Artistry Hub</h1>
+    <button on:click={goToCart} class="cart">
+        <img src={carts} alt="cart" width="29" height="29.02">
     </button>
 </header>
 
 <style>
+.shop {
+    cursor: pointer;
+}
 
 .cart {
     cursor: pointer;
@@ -24,4 +40,5 @@
     margin-right: 1rem;
     margin-left: auto;
 }
+
 </style>
